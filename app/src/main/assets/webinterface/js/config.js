ed2a44ecb640f6ddc67383c389492ae0e2f21fe6
Marwin Dollendorf <marwin.dollendorf@gmail.com>
2023-03-24 12:51:13 +0100

    Added experiment selector in webinterface configuration.

--- app/src/main/assets/webinterface/js/config.js
+++ app/src/main/assets/webinterface/js/config.js
@@ -2,16 +2,20 @@ var vm = new Vue({
   el: '#app',
   data: {
     config: {
-        "experiment": "experiment.json",
-        "language": "de_DE",
-        "speech_speed": 75,
-        "speech_pitch": 80
-    }
+        experiment: "experiment.json",
+        language: "de_DE",
+        speech_speed: 75,
+        speech_pitch: 80
+    },
+    experiments: null
   },
   methods: {
     loadConfig: async function() {
         var dataResponse = await fetch("/config");
         this.config = await dataResponse.json();
+        dataResponse = await fetch("/experiments");
+        dataResponse = await dataResponse.text();
+        this.experiments = dataResponse.split(",");
     }
   },
   mounted: function() {
